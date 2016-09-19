9ffb993e2ce86059b0bf1d1ccc6b0524b905e12b
Amath <amath@jaccede.com>
2016-09-19 16:44:27 +0200

    first commit

---
+++ frenchkit-accessibility/DetailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailsViewController.swift
+//  frenchkit-accessibility
+//
+//  Created by amath on 13/09/16.
+//  Copyright © 2016 amath. All rights reserved.
+//
+
+import Foundation
---
+++ frenchkit-accessibility/Place.swift
@@ -0,0 +1,9 @@
+//
+//  Place.swift
+//  frenchkit-accessibility
+//
+//  Created by amath on 15/09/16.
+//  Copyright © 2016 amath. All rights reserved.
+//
+
+import Foundation
